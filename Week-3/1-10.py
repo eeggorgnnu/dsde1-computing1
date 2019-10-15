aa9d0577e00fc1ee638ff1ec3b22de541ee002a3
George Gunn <george.gunn19@imperial.ac.uk>
2019-10-15 12:03:33 +0100

    Initial upload of week 3 work

---
+++ Week-3/1-10.py
@@ -0,0 +1,11 @@
+print("For loop Version")
+
+for i in range (11):
+    print(i)
+
+print("While loop Version")
+
+counter = 0
+while counter < 11:
+    print (counter)
+    counter +=1
---
+++ Week-3/10-1.py
@@ -0,0 +1,11 @@
+print("For loop Version")
+
+for i in range(11):
+    print(10 - i)
+
+print("while loop Version")
+
+counter = 10
+while counter > -1 :
+    print (counter)
+    counter -=1
